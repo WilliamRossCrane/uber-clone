de58cd938e851d0a00f669a3d87d7cae9e74496c
William Crane <wcran7@gmail.com>
2025-05-19 14:03:18 +1000

    Initial Commit - Installed all packages and dependencies at correct versions, this now allows Tailwind to work and styling if configured correctly.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,11 @@
-import { Text, View } from 'react-native';
-import { TailwindProvider } from 'tailwindcss-react-native';
+import { Text, StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function App() {
+export default function HomeScreen(): React.JSX.Element {
   return (
-    <TailwindProvider>
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-blue-500 text-xl">Tailwind works!</Text>
-      </View>
-    </TailwindProvider>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className={"text-red-500"}>Uber Clone!</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  presets: [require('tailwindcss-react-native/preset')],
-  content: [
-    './App.{js,jsx,ts,tsx}',
-    './src/**/*.{js,jsx,ts,tsx}',
-    './app/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
