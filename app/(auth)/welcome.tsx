e4a791843bcbf90f729cc8c6299ea61a71cad585
William Crane <wcran7@gmail.com>
2025-05-23 18:45:06 +1000

    FIX: Simple fix, pathway to index.ts to retrieve the constant data and I correctly wrapped a text component

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
 import Swiper from "react-native-swiper";
 import { useRef, useState } from "react";
-import { onboarding } from "@/constants";
+import { onboarding } from "../constants";
 
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
