ef572a28129fff742f4e0b381a8a5c563437153a
William Crane <wcran7@gmail.com>
2025-05-29 07:34:31 +1000

    Auth Screens: Worked on them further specifically with the Input fields for the signup form.

--- app/(auth)/components/InputField.tsx
+++ app/(auth)/components/InputField.tsx
@@ -1,5 +1,21 @@
-import { KeyboardAvoidingView } from "react-native";
+import { Label } from "@react-navigation/elements";
+import {
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from "react-native";
 
-const InputField = () => <KeyboardAvoidingView></KeyboardAvoidingView>;
+const InputField = ({ label, labelStyle }) => (
+  <KeyboardAvoidingView>
+    <TouchableWithoutFeedback>
+      <View className="my-2 w-full">
+        <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>
+          {label}
+        </Text>
+      </View>
+    </TouchableWithoutFeedback>
+  </KeyboardAvoidingView>
+);
 
 export default InputField;
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,9 +1,15 @@
 import { Text, ScrollView, View, Image } from "react-native";
-import { images } from "../constants";
-
+import { images, icons } from "../constants";
 import InputField from "./components/InputField";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
@@ -15,7 +21,18 @@ const SignUp = () => {
         </View>
 
         <View className="p-5">
-          <InputField />
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) =>
+              setForm({
+                ...form,
+                name: value,
+              })
+            }
+          />
         </View>
       </View>
     </ScrollView>
